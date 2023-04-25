0d65a8299dfe01dff2f06767aba1cceebc15deeb
Brayhann <brayhan.laiton@gmail.com>
2023-04-25 17:40:40 -0500

    Entrega de los router-links de la pag hasta el momento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -7,6 +7,30 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RegistroComponent } from './registro/registro.component';
+import { RouterModule, Routes, } from '@angular/router';
+
+
+const routes: Routes = [
+      {
+      path: '',
+      component: HomeComponent
+      },
+
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+
+      {
+        path:'login',
+        component: LoginComponent
+      },
+
+      {
+        path:'registro',
+        component: RegistroComponent
+      },
+];
 
 @NgModule({
   declarations: [
@@ -21,7 +45,8 @@ import { RegistroComponent } from './registro/registro.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
