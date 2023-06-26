9836cf93e4961886d913810dfcd1511e49f42283
Brayhann <brayhan.laiton@gmail.com>
2023-06-26 08:04:27 -0500

    Product potision

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent {
     this.formatoLogin = new FormGroup(
       {
         email: new FormControl('', [Validators.required, Validators.pattern("^[^ ]+@[^ ]+\.[a-z]{2,6}$")]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6),Validators.maxLength(20)])
+        password: new FormControl('', [Validators.required, Validators.minLength(8),Validators.maxLength(20)])
       }
     )
   }
